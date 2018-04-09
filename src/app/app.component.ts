a30229e5358cf3a61445e85f4b98e6272b217cc0
Anthony Lyons <anthony@thelyons.me>
2018-04-09 11:19:55 -0400

    Added geolocation to app. Currently have to click the page in order to get the current location.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
 import { latLng, tileLayer } from 'leaflet';
 
-
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,13 +11,26 @@ export class AppComponent implements OnInit {
 
   options = {};
 
+  center = latLng(46.879966, -121.726909);
+
+  zoom = 5;
+
+
   ngOnInit(){
+    console.log("Our current position");
+    var scope = this;
+    navigator.geolocation.getCurrentPosition(function(position){
+      var lat = position.coords.latitude;
+      var long = position.coords.longitude
+
+      scope.center = latLng(lat, long);
+      scope.zoom = 60;
+    });
+
     this.options = {
     layers: [
     tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
-    ],
-    zoom: 5,
-    center: latLng(46.879966, -121.726909)
+    ]
     };
   }
 }
